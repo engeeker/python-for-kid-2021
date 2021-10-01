53adf847c85cd1a2d49166c6a939bf1e3717c95a
Peng Xiao <xiaoquwl@gmail.com>
2021-10-01 15:07:54 +0200

    Create name_random.py

---
+++ 2021-06-05/name_random.py
@@ -0,0 +1,33 @@
+import turtle
+import random
+import time
+
+turtle.hideturtle()
+p = turtle.Pen()
+p.hideturtle()
+screen = turtle.Screen()
+screen.tracer(False)
+
+state_num = 0
+global name
+
+name_list = ['A', 'B', 'C', 'D', 'E']
+
+
+def advance_state_machine():
+    global state_num
+    if state_num == 0:
+        state_num = 1
+    else:
+        state_num = 0
+
+
+screen.onkey(advance_state_machine, "space")
+
+screen.listen()
+while True:
+    if state_num == 0:
+        name = random.choice(name_list)
+    p.write(name, font=("Arial", 100, "normal"))
+    time.sleep(0.01)  # 暂定0.01秒
+    p.clear()  # 清理，擦除
